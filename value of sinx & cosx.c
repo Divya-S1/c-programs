0c3403479a972bdd14d1edfd8edb0d690ff86dc8
Divya-S1 <62104448+Divya-S1@users.noreply.github.com>
2020-03-13 17:32:58 +0530

    Create value of sinx & cosx.c

---
+++ value of sinx & cosx.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+    {   
+      double sin value,cos value,x;
+      printf("\n Enter the value:");
+      scanf("%f",&x);
+      sin value =sin(x);
+      cos value =cos(x);
+      printf("\n the sin value of %f=%f",x,sin value);
+      printf("\n the cos value of %f=%f",x,cos value);
+    }  
